e78e90e9dd80f0ccecdc1742d2e420d9ad4b92d0
francesco di stefano <ceccio@MacBook-Air-di-francesco-2.local>
2016-03-01 16:18:24 +0100

    first commit

---
+++ myApp2/www/js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('app.controllers', [])
+
+.controller('homeCtrl', function($scope) {
+
+})
+
+.controller('page2Ctrl', function($scope) {
+
+})
+
+.controller('page3Ctrl', function($scope) {
+
+})
+
+.controller('page4Ctrl', function($scope) {
+
+})
+
+.controller('page5Ctrl', function($scope) {
+
+})
+
+.controller('page6Ctrl', function($scope) {
+
+})
+
+.controller('page7Ctrl', function($scope) {
+
+})
+
+.controller('page8Ctrl', function($scope) {
+
+})
